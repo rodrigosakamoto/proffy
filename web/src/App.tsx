7f92208f8240e69c5538daa639011578c9d33014
rodrigosakamoto <rodosakamoto@gmail.com>
2020-08-03 12:27:12 -0300

    feat: landing page

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Landing from './pages/Landing';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Landing />
+    </>
+  );
+}
+
+export default App;
