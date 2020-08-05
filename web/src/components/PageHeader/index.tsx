93b928d355c24a9def06cd2b8e1dad62c02a0e7e
rodrigosakamoto <rodosakamoto@gmail.com>
2020-08-05 10:19:31 -0300

    feat: TeacherForm page

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -9,8 +9,13 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  children,
+  description,
+}) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -22,6 +27,8 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
+
         {children}
       </div>
     </header>
